e01d7776dbd4bbf6b917300681869a278ba0f67f
Astaro212 <astaroraksion@gmail.com>
2026-04-10 01:16:51 +0300

    Manages Items of Plugins

--- src/main/java/com/astaro/mixGame/services/ItemService.java
+++ src/main/java/com/astaro/mixGame/services/ItemService.java
@@ -1,4 +1,24 @@
 package com.astaro.mixGame.services;
 
+import com.astaro.mixGame.MixGame;
+import net.kyori.adventure.text.Component;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class ItemService {
+
+    private final MixGame plugin;
+
+    public ItemService(MixGame plugin) {
+        this.plugin = plugin;
+    }
+
+    public ItemStack getLeaveItem(){
+        ItemStack stack = new ItemStack(plugin.getSettings().leaveItem());
+        ItemMeta meta = stack.getItemMeta();
+        meta.displayName(Component.text("Покинуть арену"));
+        stack.setAmount(1);
+        stack.setItemMeta(meta);
+        return stack;
+    }
 }
