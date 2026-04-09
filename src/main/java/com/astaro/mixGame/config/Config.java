35e0d4134a500f67c27db0a3c97bc4f4add179f8
Astaro212 <astaroraksion@gmail.com>
2026-04-10 01:13:27 +0300

    Record for config. I decided to use this, because it can support needed settings for services without calling configLoader and YAML.

--- src/main/java/com/astaro/mixGame/config/Config.java
+++ src/main/java/com/astaro/mixGame/config/Config.java
@@ -1,4 +1,46 @@
 package com.astaro.mixGame.config;
 
-public record Config() {
+import org.bukkit.Material;
+
+import java.util.List;
+
+public record Config(
+        String configVersion,
+        String lang,
+        BossBarSettings bossBar,
+        int lobbyCountdown,
+        int gameCountdown,
+        DatabaseSettings db,
+        List<String> whitelistedCommands,
+        boolean expBarEnabled,
+        int pointsToLevel,
+        boolean saveInventory,
+        boolean saveExp,
+        boolean scoreboardEnabled,
+        boolean tabListEnabled,
+        boolean titleTextEnabled,
+        RewardsSettings rewards,
+        Material leaveItem,
+        SongSettings songs
+) {
+    public record BossBarSettings(boolean enabled, String color) {
+    }
+
+    public record DatabaseSettings(
+            String type, String host, String database,
+            String username, String password, int port, String table
+    ) {
+    }
+
+    public record RewardsSettings(
+            boolean enabled,
+            List<String> perRound,
+            List<String> perGame,
+            List<String> winner
+    ) {
+    }
+
+    public record SongSettings(boolean lobby, boolean arena) {
+    }
 }
+
